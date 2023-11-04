e9b8946e155d66d012bc6efb522c20e0a4454274
ghulammujtaba153 <ghulammujtaba.dro@gmail.com>
2023-11-04 19:10:02 +0500

    commit2

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function toggleMenu() {
     var menu = document.getElementById('nav-container');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
+    var icon = document.getElementById('icon');
+    if (menu.style.height === '0px' || menu.style.height === '') {
+        menu.style.height = '250px';
+        menu.style.opacity = '1';
+        icon.src = 'assets/icons8-cross-50.png'; 
     } else {
-        menu.style.display = 'block';
+        menu.style.height = '0';
+        menu.style.opacity = '0';
+        icon.src = 'assets/icons8-menu-50.png'; 
     }
 }
